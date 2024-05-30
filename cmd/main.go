8af658843a5e490f9b1feae6cd67a2bcc1461f5b
Maxim Bordea <maxbordea@gmail.com>
2024-05-30 21:48:11 +0300

    add config files and viper initialization

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"log"
 
-	"github.com/maximus969/users-app"	
+	"github.com/maximus969/users-app"
+	"github.com/maximus969/users-app/pkg/handler"
 	"github.com/maximus969/users-app/pkg/repository"
 	"github.com/maximus969/users-app/pkg/service"
-	"github.com/maximus969/users-app/pkg/handler"
+	"github.com/spf13/viper"
 )
 
 func main () {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
@@ -17,7 +22,13 @@ func main () {
 
 	srv := new(users.Server)	
 
-	if error := srv.Run("8000", handlers.InitRoutes()); error != nil {
+	if error := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); error != nil {
 		log.Fatalf("error occured while running http server: %s", error.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
